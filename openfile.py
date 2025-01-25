ac219f772280369afa04960ebec6039ce90f7a17
Voronkov68 <158170491+Voronkov68@users.noreply.github.com>
2025-01-25 19:29:28 +0300

    sds

--- openfile.py
+++ openfile.py
@@ -3,5 +3,17 @@
 filename = 'pi.txt'
 
 with open(filename) as f:
-	for line in f:
-		print(line)
+	lines = f.readlines()   
+print(lines)
+pi_string = ''
+for line in lines:
+	pi_string += line.rstrip()
+
+print(pi_string)
+print(len(pi_string))  
+
+
+try:
+	print(3 / 0)
+except ZeroDivisionError:
+	print('You can t divide by zero')
